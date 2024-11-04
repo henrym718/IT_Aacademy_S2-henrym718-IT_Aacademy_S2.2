a616b3aaf793b9534e4349afb3181d7d1d91c6ff
Henry Mosquera <henrym.718@hotmail.com>
2024-11-04 11:59:48 +0100

    Entrega de srimp 2.2.

--- js/checkout.js
+++ js/checkout.js
@@ -1,28 +1,87 @@
-
 // Exercise 6
-function validate() {
-	var error = 0;
-	// Get the input fields
-	var fName = document.getElementById("fName");
-	var fEmail = document.getElementById("fEmail");
+function validate(event) {
+  event.preventDefault();
 
-	// Get the error elements
-	var errorName = document.getElementById("errorName");
-	var errorEmail = document.getElementById("errorEmail");  
-	
-	// Validate fields entered by the user: name, phone, password, and email
-	if(fName.value == ""){
-		error++;
-	}
+  var error = 0;
+  // Get the input fields
+  var fName = document.getElementById("fName");
+  var fLastN = document.getElementById("fLastN");
+  var fEmail = document.getElementById("fEmail");
+  var ffPassword = document.getElementById("fPassword");
+  var fAddress = document.getElementById("fAddress");
+  var fPhone = document.getElementById("fPhone");
 
-	if(fEmail.value == ""){
-		error++;
-	}
-	 
-	if(error>0){
-		alert("Error");
-	}else{
-		alert("OK");
-	}
+  // Get the error elements
+  var errorName = document.getElementById("errorName");
+  var errorLastN = document.getElementById("errorLastN");
+  var errorEmail = document.getElementById("errorEmail");
+  var errorPassword = document.getElementById("errorPassword");
+  var errorAddress = document.getElementById("errorAddress");
+  var errorPhone = document.getElementById("errorPhone");
 
+  // regex validator
+  const validators = {
+    name: /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]{3,}$/,
+    lastN: /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]{3,}$/,
+    email: /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/,
+    password: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]+$/,
+    address: /^.{3,}$/,
+    phone: /^\d{9}$/,
+  };
+
+  // Reset error in inputs
+  fName.classList.remove("is-invalid");
+  fLastN.classList.remove("is-invalid");
+  fEmail.classList.remove("is-invalid");
+  ffPassword.classList.remove("is-invalid");
+  fAddress.classList.remove("is-invalid");
+  fPhone.classList.remove("is-invalid");
+
+  // Validate fields entered by the user: name, phone, password, and email
+  if (!validators.name.test(fName.value)) {
+    fName.classList.add("is-invalid");
+    errorName.innerText =
+      "This field is required and must have, at least, 3 characters";
+    error++;
+  }
+
+  if (!validators.lastN.test(fLastN.value)) {
+    fLastN.classList.add("is-invalid");
+    errorLastN.innerText =
+      "This field is required and must have, at least, 3 characters";
+    error++;
+  }
+
+  if (!validators.email.test(fEmail.value)) {
+    fEmail.classList.add("is-invalid");
+    errorEmail.innerText =
+      "This field is required and must contain an '@' and have, at least, 3 characters";
+    error++;
+  }
+
+  if (!validators.password.test(ffPassword.value)) {
+    ffPassword.classList.add("is-invalid");
+    errorPassword.innerText = "Enter a correct password";
+    error++;
+  }
+
+  if (!validators.address.test(fAddress.value)) {
+    fAddress.classList.add("is-invalid");
+    errorAddress.innerText =
+      " This field is required and must have, at least, 3 characters";
+    error++;
+  }
+
+  if (!validators.phone.test(fPhone.value)) {
+    fPhone.classList.add("is-invalid");
+    errorPhone.innerText =
+      " Invalid phone number!! Must be 9 digits with no letters";
+    error++;
+  }
+
+  if (error > 0) {
+    alert("Error");
+  } else {
+    alert("OK");
+  }
 }
--- js/shop.js
+++ js/shop.js
@@ -1,72 +1,72 @@
 // If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional
 var products = [
-    {
-        id: 1,
-        name: 'cooking oil',
-        price: 10.5,
-        type: 'grocery',
-        offer: {
-            number: 3,
-            percent: 20
-        }
+  {
+    id: 1,
+    name: "T-Shirt",
+    price: 10.5,
+    type: "Ropa de hombre",
+    offer: {
+      number: 3,
+      percent: 20,
     },
-    {
-        id: 2,
-        name: 'Pasta',
-        price: 6.25,
-        type: 'grocery'
+  },
+  {
+    id: 2,
+    name: "T-Shirt limited",
+    price: 6.25,
+    type: "Ropa de hombre",
+  },
+  {
+    id: 3,
+    name: "Jacket",
+    price: 5,
+    type: "Ropa de hombre",
+    offer: {
+      number: 10,
+      percent: 30,
     },
-    {
-        id: 3,
-        name: 'Instant cupcake mixture',
-        price: 5,
-        type: 'grocery',
-        offer: {
-            number: 10,
-            percent: 30
-        }
-    },
-    {
-        id: 4,
-        name: 'All-in-one',
-        price: 260,
-        type: 'beauty'
-    },
-    {
-        id: 5,
-        name: 'Zero Make-up Kit',
-        price: 20.5,
-        type: 'beauty'
-    },
-    {
-        id: 6,
-        name: 'Lip Tints',
-        price: 12.75,
-        type: 'beauty'
-    },
-    {
-        id: 7,
-        name: 'Lawn Dress',
-        price: 15,
-        type: 'clothes'
-    },
-    {
-        id: 8,
-        name: 'Lawn-Chiffon Combo',
-        price: 19.99,
-        type: 'clothes'
-    },
-    {
-        id: 9,
-        name: 'Toddler Frock',
-        price: 9.99,
-        type: 'clothes'
-    }
-]
+  },
+  {
+    id: 4,
+    name: "Sleepwear",
+    price: 26,
+    type: "Ropa de cama",
+  },
+  {
+    id: 5,
+    name: "Nightwear",
+    price: 20.5,
+    type: "Ropa de cama",
+  },
+  {
+    id: 6,
+    name: "Loungewear",
+    price: 12.75,
+    type: "Ropa de cama",
+  },
+  {
+    id: 7,
+    name: "Dreamland",
+    price: 15,
+    type: "Ofertas",
+  },
+  {
+    id: 8,
+    name: "Cozy Corner",
+    price: 19.99,
+    type: "Ofertas",
+  },
+  {
+    id: 9,
+    name: "Toddler Frock",
+    price: 9.99,
+    type: "Ofertas",
+  },
+];
 
-// => Reminder, it's extremely important that you debug your code. 
+// => Reminder, it's extremely important that you debug your code.
 // ** It will save you a lot of time and frustration!
-// ** You'll understand the code better than with console.log(), and you'll also find errors faster. 
+// ** You'll understand the code better than with console.log(), and you'll also find errors faster.
 // ** Don't hesitate to seek help from your peers or your mentor if you still struggle with debugging.
 
 // Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
@@ -76,38 +76,124 @@ var total = 0;
 
 // Exercise 1
 function buy(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cart array
+  const cartProduct = cart.find((product) => product?.id === id);
+  if (cartProduct) {
+    cartProduct.count = cartProduct.count + 1;
+  } else {
+    const findProduct = products.find((product) => product.id === id);
+    findProduct.count = 1;
+    cart.push(findProduct);
+  }
+  calculateSubtotalProduct();
+  applyPromotionsCart();
+  countProducts();
+
+  const addProduct = products.find((product) => product.id === id);
+  alert(`${addProduct.name} se ha añadido a la cesta`);
+}
+
+// Exercise 1.1.
+function countProducts() {
+  const countProduct = document.getElementById("count_product");
+  countProduct.textContent = cart.length;
 }
 
 // Exercise 2
 function cleanCart() {
-
+  cart.length = 0;
+  printCart();
+  countProducts();
 }
 
 // Exercise 3
-function calculateTotal() {
-    // Calculate total price of the cart using the "cartList" array
+function calculateTotalCart() {
+  const totalCart = cart.reduce(
+    (acum, producto) => acum + parseFloat(producto.subtotal),
+    0
+  );
+  return parseFloat(totalCart).toFixed(2);
+}
+
+// Ecercise 3.1
+function calculateSubtotalProduct() {
+  cart.forEach(
+    (product) =>
+      (product.subtotal = parseFloat(product.count * product.price).toFixed(2))
+  );
 }
 
 // Exercise 4
 function applyPromotionsCart() {
-    // Apply promotions to each item in the array "cart"
+  cart.forEach((product) => {
+    if (product?.offer && product.count >= product?.offer?.number) {
+      product.subtotal = parseFloat(
+        product.count * product.price * (1 - product.offer.percent / 100)
+      ).toFixed(2);
+    }
+  });
 }
 
 // Exercise 5
 function printCart() {
-    // Fill the shopping cart modal manipulating the shopping cart dom
-}
+  const cartList = document.getElementById("cart_list");
+  const totalPrice = document.getElementById("total_price");
 
+  let row = "";
+
+  cart.forEach((producto, index) => {
+    row += `
+               <tr>
+                  <th scope="row">${producto.name}</th>
+                  <td>$${producto.price}</td>
+                  <td>${producto.count}</td>
+                  <td>${producto.subtotal}</td>
+                  <td>
+                    <button
+                      type="button"
+                      onclick="addFromCart(${index})"
+                      class="btn btn-outline-dark"
+                    >
+                      +
+                    </button>
+                    <button
+                      type="button"
+                      onclick="removeFromCart(${index})"
+                      class="btn btn-outline-dark"
+                    >
+                      -
+                    </button>
+                  </td>
+                </tr>
+  
+  `;
+  });
+  cartList.innerHTML = row;
+  totalPrice.textContent = calculateTotalCart();
+}
 
 // ** Nivell II **
 
 // Exercise 7
-function removeFromCart(id) {
+function addFromCart(index) {
+  cart[index].count = cart[index].count + 1;
+  calculateSubtotalProduct();
+  applyPromotionsCart();
+  printCart();
+  calculateTotalCart();
+}
 
+function removeFromCart(index) {
+  cart[index].count = cart[index].count - 1;
+  if (cart[index].count === 0) {
+    cart.splice(index, 1);
+  }
+  calculateSubtotalProduct();
+  applyPromotionsCart();
+  printCart();
+  calculateTotalCart();
+  countProducts();
 }
 
 function open_modal() {
-    printCart();
+  printCart();
 }
